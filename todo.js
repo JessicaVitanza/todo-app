07f22171cc1c3a1a3cdd51bec1236e041d6ba362
JessicaVitanza <103486794+JessicaVitanza@users.noreply.github.com>
2022-05-26 08:51:20 +0200

    css

--- todo.js
+++ todo.js
@@ -1,31 +1,45 @@
+// function parseUrlParams(){
+//   const url = window.location.href;
+//   console.log('URL', url);
+//   const urlArray = url.split('?');
+//   console.log('URLarray', urlArray);
+//   const paramsString = urlArray[1];
+//   console.log('secondo elemento di urlarray', paramsString);
+//   if (paramsString) {
+//     const paramsArray = paramsString.split('&');
+//     console.log('array dei parametri', paramsArray)
+//     const paramsObj = {};
+//     for (const str of paramsArray) {
+//       console.log('stringa parametro', str)
+//       const strArray = str.split('=')
+//       console.log('array del parametro', strArray)
+//       paramsObj[strArray[0]] = decodeURIComponent(strArray[1]) ;
+//     }
+//     console.log('paramsObj', paramsObj)
+//   } else {
+//     return null;
+//   }
+// }
 
 function parseUrlParams() {
-  const url = window.location.href;
-  const urlArray = url.split('?');
-  const paramsString = urlArray[1];
-  if (paramsString) {
-    const paramsArray = paramsString.split('&');
-    const paramsObj = {};
-    for (const string of paramsArray){
-        const stringArray = string.split('=');
-        paramsObj[stringArray[0]] = decodeURIComponent(stringArray[1]);
-    }
-    console.log(paramsObj);
-  } else {
-    return null;  
-  } 
+  const urlSearchParams = new URLSearchParams(window.location.search);
+  const params = Object.fromEntries(urlSearchParams.entries());
+  return params;
+  //console.log('params', params);
 }
 
 const params = parseUrlParams();
+
 console.log(params);
 
-
 // function getTodoFromSessionStorage(){
-//   const todoString = sessionStorage.getItem('selctedTodo');
+//   const todoString = sessionStorage.getItem('selectedTodo');
 //   if (todoString) {
-//       const todo = JSON.parse(todoString);
-//     console.log(todo);
+//     const todo = JSON.parse(todoString);
+//     console.log('todo', todo)
 //   }
 // }
-// getTodoFromSessionStorage();
+
+
+// getTodoFromSessionStorage()
 
